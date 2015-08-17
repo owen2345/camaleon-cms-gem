83b08cdc5ab16ef4f00d8f5f96813af04ca268bf
Owen <owenperedo@gmail.com>
2015-08-17 19:22:58 -0400

    rebuilt admin menus and removed nokogiri gem

--- app/helpers/admin/menus_helper.rb
+++ app/helpers/admin/menus_helper.rb
@@ -12,22 +12,22 @@ module Admin::MenusHelper
   def admin_menus_add_commons
     admin_menu_add_menu("dashabord", {icon: "dashboard", title: t('admin.sidebar.dashboard'), url: admin_dashboard_path})
     #if can? :manager, :content
-      items = []
+    items = []
 
-      current_site.post_types.eager_load(:metas).visible_menu.all.each do |pt|
-        pt = pt.decorate
-        items_i = []
-        items_i << {icon: "list", title: "#{t('admin.post_type.all')}", url: admin_post_type_posts_path(pt.id)} if can? :posts, pt
-        items_i << {icon: "plus", title: "#{t('admin.post_type.add_new')}  ", url: new_admin_post_type_post_path(pt.id)} if can? :create_post, pt
-        if pt.manage_categories?
-          items_i << {icon: "folder-open", title: t('admin.post_type.categories'), url: admin_post_type_categories_path(pt.id)} if can? :categories, pt
-        end
-        if pt.manage_tags?
-          items_i << {icon: "tags", title: t('admin.post_type.tags'), url: admin_post_type_post_tags_path(pt.id)} if can? :post_tags, pt
-        end
-        items << {icon: "file-o", title: pt.the_title, url: "", items: items_i} if items_i.present? #if can? :posts, pt
+    current_site.post_types.eager_load(:metas).visible_menu.all.each do |pt|
+      pt = pt.decorate
+      items_i = []
+      items_i << {icon: "list", title: "#{t('admin.post_type.all')}", url: admin_post_type_posts_path(pt.id)} if can? :posts, pt
+      items_i << {icon: "plus", title: "#{t('admin.post_type.add_new')}  ", url: new_admin_post_type_post_path(pt.id)} if can? :create_post, pt
+      if pt.manage_categories?
+        items_i << {icon: "folder-open", title: t('admin.post_type.categories'), url: admin_post_type_categories_path(pt.id)} if can? :categories, pt
       end
-      admin_menu_add_menu("content", {icon: "copy", title: t('admin.sidebar.content'), url: "", items: items}) if items.present?
+      if pt.manage_tags?
+        items_i << {icon: "tags", title: t('admin.post_type.tags'), url: admin_post_type_post_tags_path(pt.id)} if can? :post_tags, pt
+      end
+      items << {icon: "file-o", title: pt.the_title, url: "", items: items_i} if items_i.present? #if can? :posts, pt
+    end
+    admin_menu_add_menu("content", {icon: "copy", title: t('admin.sidebar.content'), url: "", items: items}) if items.present?
     #end
 
     admin_menu_add_menu("media", {icon: "picture-o", title: t('admin.sidebar.media'), url: admin_media_path}) if can? :manager, :media
@@ -116,36 +116,66 @@ module Admin::MenusHelper
   # draw admin menu as html
   def admin_menu_draw
     res= []
-    @_admin_menus.each do |key, menu|
-      res << "<li data-key='#{key}' class='#{"xn-openable" if menu.has_key?(:items)} #{_admin_menu_check_url(menu[:url])}'>
+    @_tmp_menu_parents = []
+    menus = _get_url_current
+    menus.each do |menu|
+      res << "<li data-key='#{menu[:key]}' class='#{"xn-openable" if menu.has_key?(:items)} #{'active' if is_active_menu(menu[:key])}'>
         <a href='#{menu[:url]}'><span class='fa fa-#{menu[:icon]}'></span> <span class='xn-text'>#{menu[:title]}</span></a>
         #{_admin_menu_draw(menu[:items]) if menu.has_key?(:items)}
       </li>"
     end
-
-    doc = Nokogiri::HTML.fragment(res.join)
-    link_active = doc.css(".active").first
-    link_active1 = doc.css(".parent_active1").first
-    link_active2 = doc.css(".parent_active2").first
-    link_active3 = doc.css(".parent_active3").first
-    if link_active.present?
-      _admin_menu_draw_active(link_active)
-    elsif link_active1.present?
-      _admin_menu_draw_active(link_active1)
-    elsif link_active2.present?
-      _admin_menu_draw_active(link_active2)
-    elsif link_active3.present?
-      _admin_menu_draw_active(link_active3)
-    end
-    doc.to_html
+    _admin_menu_draw_active
+    res.join
   end
 
   private
+
+  def _get_url_current
+    menus = @_admin_menus.map{|key, menu|  menu[:key] = key; menu}
+    c_site = site_current_url.sub("https://", "http://")
+    current_path = "/#{c_site.split(root_url).last}"
+    current_path_array = current_path.split('/')
+    a_size = current_path_array.size
+
+    (0..a_size).each do |i|
+      resp = _search_in_menus(menus, current_path_array[0..a_size-i].join('/'))
+      bool = resp[:bool]
+      menus = resp[:menus]
+      break if bool
+    end
+    menus
+  end
+
+  def is_active_menu(key)
+    @_tmp_menu_parents.map{|item| item[:key] == key}.include?(true)
+  end
+
+  def _search_in_menus(menus, _url, parent_index = 0)
+    bool = false
+    menus.each_with_index do |menu, index_menu|
+      menu[:key] = "#{parent_index}__#{rand(999...99999)}" if menu[:key].nil?
+      url = menu[:url].to_s.sub("https://", "http://")
+      url_path = url
+      url_path = "/#{url.split(root_url).last}" if url.start_with?("http://")
+      bool = url_path == _url
+      if menu.has_key?(:items)
+        resp = _search_in_menus(menu[:items], _url, parent_index + 1)
+        bool = bool || resp[:bool]
+        menu[:items] = resp[:menus]
+      end
+      if bool
+        @_tmp_menu_parents[parent_index] = {url: menu[:url], title: menu[:title], key: menu[:key]}
+        break
+      end
+    end
+    {menus: menus, bool: bool}
+  end
+
   def _admin_menu_draw(items)
     res = []
     res  << "<ul>"
     items.each do |item|
-      res  << "<li class='#{"xn-openable" if item.has_key?(:items)} #{_admin_menu_check_url(item[:url])}'>
+      res  << "<li class='#{"xn-openable" if item.has_key?(:items)} #{'active' if is_active_menu(item[:key])}'>
                 <a href='#{item[:url]}'><span class='fa fa-#{item[:icon]}'></span> #{item[:title]}</a>
                 #{_admin_menu_draw(item[:items]) if item.has_key?(:items)}
               </li>"
@@ -154,40 +184,12 @@ module Admin::MenusHelper
     res.join
   end
 
-  def _admin_menu_check_url(url)
-    url = url.to_s.sub("https://", "http://")
-
-    c_site = site_current_url.sub("https://", "http://")
-    current_path = "/#{c_site.split(root_url).last}"
-
-    url_path = url
-    url_path = "/#{url.split(root_url).last}" if url.start_with?("http://")
-
-    # primary menu
-    if c_site == url || current_path == url
-      res = "active"
-    elsif url_path.split("/").slice(1, 4) == current_path.split("/").slice(1, 4)
-      res = "parent_active1"
-    elsif url_path.split("/").slice(1, 3) == current_path.split("/").slice(1, 3)
-        res = "parent_active2"
-    elsif url_path.split("/").slice(1, 2) == current_path.split("/").slice(1, 2)
-        res = "parent_active3"
-    end
-    res
-
-  end
-
-  def _admin_menu_draw_active(link_active)
-    a = link_active.children.search("a").first
+  def _admin_menu_draw_active
     bread = []
-    bread << [a.content, a["href"]]
-    link_active['class'] += " active "
-    link_active.ancestors('li').each do |parent|
-      parent['class'] += ' active parent-active'
-      a = parent.children.search("a").first
-      bread << [a.content, a["href"]]
+    @_tmp_menu_parents.each do |item|
+      bread << [ActionView::Base.full_sanitizer.sanitize(item[:title]), item[:url]] if item.present?
     end
-    @_admin_breadcrumb = [[t('admin.sidebar.dashboard'), admin_dashboard_path]] + bread.reverse + @_admin_breadcrumb
+    @_admin_breadcrumb = [[t('admin.sidebar.dashboard'), admin_dashboard_path]] + bread + @_admin_breadcrumb
   end
 
 end
